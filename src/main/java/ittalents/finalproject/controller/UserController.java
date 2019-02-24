989cb55d30a7929eef60b2afd9f66a293c3d0a71
yoana <ymancheva@yahoo.com>
2019-02-24 14:50:47 +0200

    intial project

---
+++ src/main/java/ittalents/finalproject/controller/UserController.java
@@ -0,0 +1,9 @@
+package ittalents.finalproject.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+    
+}
