028a85f525ace45fd2109954cf60f5bcea2ca0ea
GerganaTaneva <gerganataneva33@gmail.com>
2019-02-26 23:41:57 +0200

    modified PetController

--- src/main/java/ittalents/finalproject/controller/PetController.java
+++ src/main/java/ittalents/finalproject/controller/PetController.java
@@ -18,13 +18,6 @@ public class PetController {
     @PostMapping(value = "/pets/add")
     public void add(HttpServletRequest req, HttpServletResponse resp){
         try {
-//            StringBuilder text = new StringBuilder();
-//            Scanner sc = new Scanner(req.getReader());
-//            while(sc.hasNextLine()){
-//                text.append(sc.nextLine());
-//            }
-//            String request = text.toString();
-//            JsonObject json = new JsonParser().parse(request).getAsJsonObject();
             String gender = req.getParameter("genderPet");
             int age = Integer.getInteger(req.getParameter("age")).intValue();
             String breed = req.getParameter("breed");
@@ -33,8 +26,8 @@ public class PetController {
             Boolean inSale = Boolean.getBoolean(req.getParameter("inSale"));
             Integer quantity = Integer.getInteger(req.getParameter("quantity"));
             Double price = Double.valueOf(req.getParameter("price"));
-            Pet p = new Pet(gender, breed, age, subBreed, description, inSale, price, quantity);
-            if(validatePet(p)){
+            if(validatePet(gender, age, breed, subBreed, description, inSale, quantity, price)){
+                Pet p = new Pet(gender, breed, age, subBreed, description, inSale, price, quantity);
                 dao.addPet(p);
                 resp.getWriter().append("The pet with id " + p.getId() + " was successfully added");
             }
@@ -43,16 +36,17 @@ public class PetController {
                 resp.getWriter().append("There are some problems adding a pet, Try again later");
             }
         } catch (Exception e) {
-            System.out.println("OPSI, " + e.getClass().getName());
+            System.out.println("OPSI, " + e.getClass().getName() + " " + e.getMessage());
         }
     }
 
-    public static boolean validatePet(Pet pet){
+    public static boolean validatePet(String gender, int age, String breed, String subBreed, String description, Boolean inSale, int quantity, double price){
         //TODO more validations for user
-        if(pet.getGenderPet() == null || pet.getAge() == 0 || pet.getBreed() == null || pet.getDescription() == null || pet.getPrice() == 0 || pet.getSubBreed() == null || pet.getQuantity() == 0){
+        if(gender == null || age < 0 || breed == null || subBreed == null || description == null || quantity < 1 || price < 0){
             return false;
         }
-        else
+        else {
             return true;
+        }
     }
 }
--- src/main/java/ittalents/finalproject/model/daos/PetDao.java
+++ src/main/java/ittalents/finalproject/model/daos/PetDao.java
@@ -2,6 +2,10 @@ package ittalents.finalproject.model.daos;
 
 import ittalents.finalproject.model.DBManager;
 import ittalents.finalproject.model.pojos.pets.Pet;
+import net.bytebuddy.dynamic.scaffold.MethodRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -9,23 +13,21 @@ import java.sql.*;
 @Component
 public class PetDao {
 
-//    @Autowired
-//    private JdbcTemplate db;
+    @Autowired
+    private JdbcTemplate db;
 
     public void addPet(Pet pet) throws Exception {
 
         String insertPet = "INSERT INTO pets(gender, breed, sub_breed, age, pet_desc, in_sale, price, quantity) VALUES(?, ?, ?, ?, ?, ?, ?, ?);";
-        PreparedStatement ps = DBManager.getInstance().getConnection().prepareStatement(insertPet, Statement.RETURN_GENERATED_KEYS);
-
-        ps.setString(1, pet.getGenderPet());
-        ps.setString(2, pet.getBreed());
-        ps.setString(3, pet.getSubBreed());
-        ps.setInt(4, pet.getAge());
-        ps.setString(5, pet.getDescription());
-        ps.setBoolean(6, pet.isInSale());
-        ps.setDouble(7, pet.getPrice());
-        ps.setInt(8, pet.getQuantity());
-
-        ps.executeUpdate();
+        db.update(insertPet, (PreparedStatement ps) -> {
+                                    ps.setString(1, pet.getGenderPet());
+                                    ps.setString(2, pet.getBreed());
+                                    ps.setString(3, pet.getSubBreed());
+                                    ps.setInt(4, pet.getAge());
+                                    ps.setString(5, pet.getDescription());
+                                    ps.setBoolean(6, pet.isInSale());
+                                    ps.setDouble(7, pet.getPrice());
+                                    ps.setInt(8, pet.getQuantity());
+        });
     }
 }
