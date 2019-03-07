a725deee147b0946815a042effa6664b55147adc
yoana <ymancheva@yahoo.com>
2019-03-07 18:55:16 +0200

    added show user's favourites products dto

--- src/main/java/ittalents/finalproject/controller/FavouriteProductController.java
+++ src/main/java/ittalents/finalproject/controller/FavouriteProductController.java
@@ -2,7 +2,9 @@ package ittalents.finalproject.controller;
 
 
 import ittalents.finalproject.model.pojos.User;
+import ittalents.finalproject.model.pojos.dto.ShowFavouriteProductsDTO;
 import ittalents.finalproject.model.pojos.products.FavouriteProduct;
+import ittalents.finalproject.model.pojos.products.Product;
 import ittalents.finalproject.model.repos.FavouriteProductRepository;
 import ittalents.finalproject.model.repos.ProductRepository;
 import ittalents.finalproject.model.repos.UserRepository;
@@ -15,8 +17,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
+import static java.lang.Math.toIntExact;
 
 @RestController
 public class FavouriteProductController extends BaseController{
@@ -31,13 +36,13 @@ public class FavouriteProductController extends BaseController{
     public void addProductToFavourite(@RequestParam("id") long id, HttpSession session) throws BaseException {
         validateLogin(session);
         User user = (User)session.getAttribute(LOGGED_USER);
-
-        if(productRepository.findById(id).isPresent()) {
+        Optional<Product> product = productRepository.findById(id);
+        if(product.isPresent()) {
             FavouriteProduct favouriteProduct = new FavouriteProduct();
             FavouriteProduct.FavouriteProductPK  pk = new FavouriteProduct.FavouriteProductPK();
 
-            pk.setUserId(user.getId());
-            pk.setProductId(id);
+            pk.setUser(user);
+            pk.setProduct(product.get());
 
             favouriteProduct.setFavouriteProductPK(pk);
             favouriteProductRepository.save(favouriteProduct);
@@ -64,9 +69,27 @@ public class FavouriteProductController extends BaseController{
     }
 
     @GetMapping(value = "products/favourites")
-    public List<FavouriteProduct> showUserFavourites(HttpSession session) throws BaseException{
+    public ShowFavouriteProductsDTO showUserFavourites(HttpSession session) throws BaseException{
         validateLogin(session);
         User user = (User)session.getAttribute(LOGGED_USER);
-        return favouriteProductRepository.getAllByFavouriteProductPK_UserId(user.getId());
+        ShowFavouriteProductsDTO userFavourites = new ShowFavouriteProductsDTO(user.getId());
+
+        userFavourites.setUsername(user.getUsername());
+
+        List<FavouriteProduct> favouriteProducts = favouriteProductRepository.getAllByFavouriteProductPK_User(user);
+        List<Product> products = new ArrayList<>();
+
+
+        for (FavouriteProduct favouriteProduct : favouriteProducts) {
+
+            Optional<Product> product = productRepository.findById(favouriteProduct.getFavouriteProductPK().getProduct().getId());
+
+            if(product.isPresent()) {
+                products.add(product.get());
+            }
+        }
+
+        userFavourites.addFavourites(products);
+        return userFavourites;
     }
 }
